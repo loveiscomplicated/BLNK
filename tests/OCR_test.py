e6699b7e4cec07699397e3ca576b554738746f48
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-04-02 16:27:43 +0900

    [UPDATE]

--- tests/OCR_test.py
+++ tests/OCR_test.py
@@ -3,8 +3,10 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from OCR import pdf_to_blanked_pdf
+from OCR import all_in_one
+input_file_path = './tests/materials/sample1.pdf'
+output_file_path = './tests/output.pdf'
+keyword_ratio = 0.000000000001
+all_in_one(input_file_path, output_file_path, keyword_ratio)
 
 
-pdf_to_blanked_pdf('./tests/materials/engmath.pdf', 0.25)
-print('good')
